81a73baceaf4eded1b4d0f510bee998de1313e0b
ShadowLp174 <samuel.moravec@yahoo.com>
2022-04-05 19:03:53 +0200

    still somehow init

--- router.js
+++ router.js
@@ -1,7 +1,19 @@
+const ws = require("ws");
+const express = require("express");
+
 // the router module to handle requests, assign jobs, etc.
 
 class Router {
   constructor() {
+    this.app = express();
+
+    this.server = this.app.listen(process.env.PORT || 3987);
+    this.wsServer = new ws.Server({ server: this.server, path: '/api/ws' });
+
+    this.wsServer.on('connection', (socket) => {
+      
+    });
+
     return this;
   }
 }
