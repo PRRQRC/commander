fabf225ab2ac50f1e24b1402b043b5f94c657f2b
Overcast <57546895+Overcast-Gaming@users.noreply.github.com>
2022-04-25 17:13:31 -0400

    fix file names and change to rmSync

--- reset.js
+++ reset.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-fs.rm(__dirname + '/e.json');
-fs.rm(__dirname + '/log.log');
-fs.writeFileSync(__dirname + '/auth/authenticated.json', JSON.stringify({}))
-fs.writeFileSync(__dirname + '/log.log', '')
+fs.rmSync(__dirname + '\\auth\\authenticated.json');
+fs.rmSync(__dirname + '\\log.log');
+fs.writeFileSync(__dirname + '\\auth\\authenticated.json', JSON.stringify({}))
+fs.writeFileSync(__dirname + '\\log.log', '')
